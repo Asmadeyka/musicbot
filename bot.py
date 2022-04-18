154aa213e9b34aadae663b19f5a939c342cabb0d
Asmadeyka <71525205+Asmadeyka@users.noreply.github.com>
2022-04-19 01:40:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3.8
 # -*- coding: utf-8 -*-
 import telebot
-import time
+from time import gmtime, strftime,localtime
 token = '1273658182:AAEY60WZtXQU0mJN90bEVTouZ7nDg1kwXFs'
 botik = telebot.TeleBot(token)
 @botik.message_handler(content_types=['text'])
 def aaa(message):
-    bbb = str(message.text)
-    botik.send_message(message.chat.id,bbb + bbb)
+    t = strftime("%H", localtime())
+    if int(t) < 8:
+        botik.send_message(message.chat.id,"спать!")
+        return
+    else:
+        try:
+            bbb = str(message.text)
+            botik.send_message(message.chat.id,bbb + bbb[0])
+        except:
+            exit()
 botik.polling()
